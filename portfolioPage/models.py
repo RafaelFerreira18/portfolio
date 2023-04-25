9d8fc88bd2ef43e5cae6df84fda3036201620929
RafaelFerreira18 <rafabf18@gmail.com>
2023-04-25 15:40:08 -0300

    finishing

--- portfolioPage/models.py
+++ portfolioPage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Message(models.Model):
-    nome = models.CharField(max_length=65, db_column="name")
+    nome = models.CharField(max_length=64, db_column="name")
     email = models.CharField(max_length=65)
     mensagem = models.CharField(max_length=5000, db_column="message")
 
